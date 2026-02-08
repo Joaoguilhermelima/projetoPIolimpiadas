94141ff4bb7a4541146af60c011112e8db305871
Joaoguilhermelima <joao.ramos@dcomp.ufs.br>
2026-02-07 23:57:51 -0300

    Transformei em uma função para usar melhor

--- output/main.c
+++ output/main.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main() 
-{
-    FILE *f = fopen("results.csv", "r");
+typedef struct { //Obter dados dos atletas
+    int ano;
+    char sexo[2];
+    char noc[4];
+    char medalha[10];
+} Atleta;
+
+int guardarDados(FILE *arquivo, Atleta *atletas) { //Função para guardar os dados dos atletas. Fica mais fácil de usar
     char linha[1024];
-
-    if (!f) {
-        printf("Erro ao abrir arquivo\n");
-        return 1;
+    
+    if (fgets(linha, sizeof(linha), arquivo) == NULL) {
+        return 0;
     }
 
-    fgets(linha, sizeof(linha), f); // Ignora o cabeçalho
-
-    while (fgets(linha, sizeof(linha), f)) { // Lê cada linha do arquivo
         char *token = strtok(linha, ",");
         int coluna = 0;
-        char sexo[10];
         char evento[100] = "";
-        char noc[4];
-        int ano;
-        char medalha[10];
 
         while (token != NULL) {
              if (coluna == 0) {
-                ano = atoi(token); // Armazenar ano. O atoi transforma string em inteiro
+                atletas->ano = atoi(token); // Armazenar ano. O atoi transforma string em inteiro
             }
             
             if (coluna == 4) { // Armazenar sexo
@@ -33,11 +30,11 @@ int main()
             } 
             
             if (coluna == 7) {
-                strcpy(noc, token); // Armazenar NOC
+                strcpy(atletas->noc, token); // Armazenar NOC
             }
 
             if (coluna == 10) {
-                strcpy(medalha, token); //Armazenar medalha
+                strcpy(atletas->medalha, token); //Armazenar medalha
             }
 
             token = strtok(NULL, ",");
@@ -45,17 +42,32 @@ int main()
         }
 
         if (strstr(evento, "Women") != NULL) {
-                strcpy(sexo, "F");
-                } else if (strstr(evento, "Men") != NULL) {
-                strcpy(sexo, "M");
-                } else {
-                strcpy(sexo, "NA");
-                }
+            strcpy(atletas->sexo, "F");
+        } else if (strstr(evento, "Men") != NULL) {
+            strcpy(atletas->sexo, "M");
+        } else {
+            strcpy(atletas->sexo, "NA");
+        }  
+    return 1;
+}
 
-        printf("%d, %s, %s, %s\n", ano, sexo, noc, medalha); // Imprime os dados extraídos
+int main() 
+{
+    FILE *arquivo = fopen("results.csv", "r");
 
+    if (!arquivo) {
+        printf("Erro ao abrir arquivo\n");
+        return 1;
     }
 
-    fclose(f);
+    char linha[1024];
+    fgets(linha, sizeof(linha), arquivo);
+    Atleta atletas;
+
+    while(guardarDados(arquivo, &atletas)) {
+        printf("%d %s %s %s\n", atletas.ano, atletas.sexo, atletas.noc, atletas.medalha);
+    }
+
+    fclose(arquivo);
     return 0;
 }
